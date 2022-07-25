a2ca573ea19865f7910a6acfbcf081bf65a0ff4e
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-24 22:00:34 -0400

    add word-break into new-tweet.css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,25 +10,13 @@
     $('.counter').text(140 -len);
     if (len > 140) {
       $('.counter').css('color', 'red')
-      // $('.alert1').show();
-      // $('.alert2').hide();
       return false; 
     } else if (len === 0) {
-      // $('.alert2').show();
-      // $('.alert1').hide();
       return false;   
     }
     else {
-      $('.counter').css('color', 'black')
-      // $('.alert2').hide();
-      // $('.alert1').hide();    
+      $('.counter').css('color', 'black')  
     }
   })
   
-})
-
-$('#tweet-text').submit(function() {
-  let content = $(this).val();
-  let len = content.length;
-  $('.counter').text(140 -len);
 })
